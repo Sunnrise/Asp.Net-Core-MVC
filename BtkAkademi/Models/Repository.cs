8962c2a561ae507d01dbe1c1113042287c5f226f
Alperen Güneş <alperenmanavgat@outlook.com>
2024-09-24 12:51:38 +0300

    Static Repository Definition

---
+++ BtkAkademi/Models/Repository.cs
@@ -0,0 +1,15 @@
+namespace BtkAkademi.Models
+{
+    public static class Repository
+    {
+        private static List<Candidate> applications= new List<Candidate>();
+        public static IEnumerable<Candidate> Applications => applications;
+
+        public static void Add(Candidate candidate)
+        {
+            applications.Add(candidate);
+        }
+        
+
+    }
+}
