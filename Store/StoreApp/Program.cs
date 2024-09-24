ec38f737b57fc76658de03d1ed84d9becfc1525a
Alperen Güneş <alperenmanavgat@outlook.com>
2024-09-24 18:10:53 +0300

    Mvc template and middleware creation

--- Store/StoreApp/Program.cs
+++ Store/StoreApp/Program.cs
@@ -1,7 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
+
+//adding controllerviews for mvc, controller for api
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
-app.MapGet("/alperen",()=> "Hello I am Alperen");
+
+app.UseHttpsRedirection();
+
+
+//for redirect http to https
+app.UseHttpsRedirection();
+
+//endpoints routes definition
+app.UseRouting();
+
+//controller route template
+app.MapControllerRoute(
+    name:"default",
+    pattern:"{controller=Home}/{action=Index}/{id?}");
+
+    //or like this
+    // "default","{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
