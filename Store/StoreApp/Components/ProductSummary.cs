43be068c5cc1fa5a6f833db9eceff1651b713618
Alperen Güneş <alperenmanavgat@outlook.com>
2024-09-29 13:29:05 +0300

    ViewComponent

---
+++ Store/StoreApp/Components/ProductSummary.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+using Repositories;
+using Services.Contracts;
+
+namespace StoreApp.Components
+{
+    public class ProductSummary: ViewComponent
+    {
+        private readonly IServiceManager _manager;
+
+        public ProductSummary(IServiceManager manager)
+        {
+            _manager = manager;
+        }
+
+        public string Invoke()
+        {
+            return _manager.ProductService.GetAllProducts(false).Count().ToString();
+        }
+    }
+}
