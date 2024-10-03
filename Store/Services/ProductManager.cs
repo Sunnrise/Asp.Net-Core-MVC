6b90f550c6312a0bdf1aae97d436f4016c182992
Alperen Güneş <alperenmanavgat@outlook.com>
2024-10-03 22:10:34 +0300

    fast forward

--- Store/Services/ProductManager.cs
+++ Store/Services/ProductManager.cs
@@ -13,6 +13,22 @@ namespace Services
             _manager = manager;
         }
 
+        public void CreateProduct(Product product)
+        {
+            _manager.Product.Create(product);
+            _manager.Save();
+        }
+
+        public void DeleteOneProduct(int id)
+        {
+            Product product=GetOneProduct(id,false);
+            if(product is not null)
+            {
+                _manager.Product.DeleteOneProduct(product);
+                _manager.Save();
+            }
+        }
+
         public IEnumerable<Product> GetAllProducts(bool trackChanges)
         {
             return _manager.Product.GetAllProducts(trackChanges);
@@ -20,10 +36,20 @@ namespace Services
 
         public Product? GetOneProduct(int id, bool trackChanges)
         {
-            var product= _manager.Product.GetOneProduct(id,trackChanges);
-            if(product is null)
+            var product = _manager.Product.GetOneProduct(id, trackChanges);
+            if (product is null)
+            {
                 throw new Exception("Product not found");
+            }
             return product;
+        }
+
+        public void UpdateOneProduct(Product product)
+        {
+            var entity = _manager.Product.GetOneProduct(product.ProductId, true);
+            entity.ProductName=product.ProductName;
+            entity.Price=product.Price;
+            _manager.Save();
         }
     }
 }
