6e8ab3c3a65d008ea700347cf92d49571200b74e
Alperen Güneş <alperenmanavgat@outlook.com>
2024-10-06 11:57:20 +0300

    Service Extension

---
+++ Store/StoreApp/Infrastructure/Extensions/ServiceExtension.cs
@@ -0,0 +1,58 @@
+using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+using Repositories;
+using Repositories.Contracts;
+using StoreApp.Models;
+
+namespace StoreApp.Infrastructure.Extensions
+{
+    public static class ServiceExtension
+    {
+        //DbExtension
+        public static void ConfigureDbContext(this IServiceCollection services, ConfigurationManager configuration)
+        {
+            //database using declaration 
+            services.AddDbContext<RepositoryContext>(options =>
+            {
+                options.UseSqlite(configuration.GetConnectionString("sqlconnection"),
+                b => b.MigrationsAssembly(nameof(StoreApp)));
+            });
+        }
+
+        //Session Extension 
+        public static void ConfigureSession(this IServiceCollection services)
+        {
+            //Session
+            services.AddDistributedMemoryCache();
+            services.AddSession(options =>
+            {
+                options.Cookie.Name = "StoreApp.Session";
+                options.IdleTimeout = TimeSpan.FromMinutes(9);
+            });
+
+            //IoC registration for Cart
+            services.AddScoped<Cart>(c => SessionCart.GetCart(c));
+
+        }
+
+        //Repository Extension 
+        public static void ConfigureRepositoryRegistration(this IServiceCollection services)
+        {
+            //Repositories
+            services.AddScoped<IRepositoryManager, RepositoryManager>();
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<IOrderRepository, OrderRepository>();
+        }
+
+        //Service Extension 
+        public static void ConfigureServiceRegistration(this IServiceCollection services)
+        {
+            //Services
+            services.AddScoped<IRepositoryManager, RepositoryManager>();
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<IOrderRepository, OrderRepository>();
+        }
+    }
+}
--- Store/StoreApp/Program.cs
+++ Store/StoreApp/Program.cs
@@ -1,11 +1,4 @@
-using Entities.Models;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Options;
-using Repositories;
-using Repositories.Contracts;
-using Services;
-using Services.Contracts;
-using StoreApp.Models;
+using StoreApp.Infrastructure.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,38 +8,24 @@ builder.Services.AddControllersWithViews();
 //adding razorpages for mvvm pattern without controller
 builder.Services.AddRazorPages();
 
-//database using declaration 
-builder.Services.AddDbContext<RepositoryContext>(options=>
-{
-    options.UseSqlite(builder.Configuration.GetConnectionString("sqlconnection"),
-    b=> b.MigrationsAssembly(nameof(StoreApp)));
-});
+//Db extension registration
+builder.Services.ConfigureDbContext(builder.Configuration);
 
-//Session
-builder.Services.AddDistributedMemoryCache();
-builder.Services.AddSession(options=>
-{
-    options.Cookie.Name="StoreApp.Session";
-    options.IdleTimeout=TimeSpan.FromMinutes(9);
-});
+//Session extension registration
+builder.Services.ConfigureSession();
+
+//Repository extension registration
+builder.Services.ConfigureRepositoryRegistration();
+
+//Service extension registration
+builder.Services.ConfigureServiceRegistration();
+
+
 
 //HttpContextAccessor IoC Regis.
 builder.Services.AddSingleton<IHttpContextAccessor,HttpContextAccessor>();
 
-//Repositories
-builder.Services.AddScoped<IRepositoryManager,RepositoryManager>();
-builder.Services.AddScoped<IProductRepository,ProductRepository>();
-builder.Services.AddScoped<ICategoryRepository,CategoryRepository>();
-builder.Services.AddScoped<IOrderRepository,OrderRepository>();
 
-//Services
-builder.Services.AddScoped<IServiceManager, ServiceManager>();
-builder.Services.AddScoped<IProductService, ProductManager>();
-builder.Services.AddScoped<ICategoryService, CategoryManager>();
-builder.Services.AddScoped<IOrderService, OrderManager>();
-
-//IoC registration for Cart
-builder.Services.AddScoped<Cart>(c=> SessionCart.GetCart(c));
 
 //AutoMapper Service Registration
 builder.Services.AddAutoMapper(typeof(Program));
